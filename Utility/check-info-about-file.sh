d911400f6bc16bd2b889dfc5bc16941520f69997
Aditya Bhardwaj <greytopihacker@gmail.com>
2020-05-13 10:40:12 +0530

    added file command - utility

---
+++ Utility/check-info-about-file.sh
@@ -0,0 +1,10 @@
+# Use 'file' command to see information about a specific file
+# for example, this is an archive - uaa-generator.tar.gz
+
+#command
+file uaa-generator.tar.gz
+
+#output
+#
+# uaa-generator.tar.gz: gzip compressed data, from Unix, last modified: Mon May 11 23:50:33 2020
+#
